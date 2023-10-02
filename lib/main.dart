10377bae53eb7163817cadc511ddf44c8d556895
Hrvoje Bartolin <hbartolin@gmail.com>
2023-10-02 13:35:23 +0200

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() => runApp(const MaterialApp(
     home: QuoteList()
@@ -27,35 +28,6 @@ class QuoteListState extends State<QuoteList> {
     Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
   ];
 
-  Widget quoteTemleate(quote) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Text(
-            quote.text,
-            style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.grey[600],
-            ),
-          ),
-            const SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-            ),
-        ],
-      ),
-      )
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,11 +39,13 @@ class QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         children: quotes.map(
-                (quote) => quoteTemleate(quote)
+                (quote) => QuoteCard(quote: quote)
         ).toList(),
       ),
     );
   }
 }
+
+
 
 
